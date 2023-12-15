a812259deb2afc15c263a2259671bcb89f6852da
rodwin02 <rohomeres026@gmail.com>
2023-12-16 05:31:51 +0800

    1231

--- inquiry.php
+++ inquiry.php
@@ -38,7 +38,7 @@ if(!empty($_GET['name'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messages</title>
     <link rel="stylesheet" href="style3.css ?<?php echo time(); ?> ">
-    <link rel="stylesheet" href="style4.css ">
+    <link rel="stylesheet" href="style4.css ?<?php echo time(); ?>  ">
     <link rel="stylesheet" href="./style/generateCert.css">
     <script src="sidebar.js "></script>
 </head>
